e841f25d5f630c000eabedaff7e3c9230aee1130
abh1sh3kk <abhishekacharya171@gmail.com>
2024-03-18 15:25:05 +0545

    did nothing

--- client/src/app/chats/page.jsx
+++ client/src/app/chats/page.jsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 import { useState, useEffect } from "react";
-import LeftSidebar from '../../components/LeftMenu'
-import ChatDetail from '../../components/ChatDetail'
+import LeftSidebar from "../../components/LeftMenu";
+import ChatDetail from "../../components/ChatDetail";
 // import LoadingScreen from "../../components/LoadingScreen";
 
-export default function Chats(){
+export default function Chats() {
   // const [progress, setProgress] = useState(0);
   // const [loading, setLoading] = useState(true);
 
@@ -20,27 +20,27 @@ export default function Chats(){
   //   return () => clearTimeout(id);
   // }, [progress]);
 
-    return(
-        <>
-        {/* {loading ? (
+  return (
+    <>
+      {/* {loading ? (
         <LoadingScreen progress={progress} />
       ) : ( */}
-             <div className="w-screen h-screen overflow-hidden bg-[#e3e1dd]">
-          {/* 2 components cointainer */}
-          <div className="flex justify-start whatsapp-bp:justify-center content-center items-center bg-[#e3e1dd] h-screen  w-[96%] shadow-xl mx-auto">
-            {/* LeftMenu */}
-            <div className="bg-[#e3e1dd]  w-full flex-2">
-              <LeftSidebar />
-              {/* <Image src={pp} alt="" /> */}
-            </div>
+      <div className="w-screen h-screen overflow-hidden bg-[#e3e1dd]">
+        {/* 2 components cointainer */}
+        <div className="flex justify-start whatsapp-bp:justify-center content-center items-center bg-[#e3e1dd] h-screen  w-[96%] shadow-xl mx-auto">
+          {/* LeftMenu */}
+          <div className="bg-[#e3e1dd]  w-full flex-2">
+            <LeftSidebar />
+            {/* <Image src={pp} alt="" /> */}
+          </div>
 
-            {/* ChatDetail */}
-            <div className="bg-[#222f35]  w-full">
-              <ChatDetail />
-            </div>
+          {/* ChatDetail */}
+          <div className="bg-[#222f35]  w-full">
+            <ChatDetail />
           </div>
         </div>
-           {/* )} */}
-        </>
-    )
+      </div>
+      {/* )} */}
+    </>
+  );
 }
