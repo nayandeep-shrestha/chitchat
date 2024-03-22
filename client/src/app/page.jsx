1a92c5cd967e1e83d51a0a5eee71c6f80c85ef29
Nischal Poudel <nischalpoudel@Nischals-MacBook-Air.local>
2024-03-22 10:52:22 +0545

    metadata, favicon

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -3,8 +3,6 @@ import { ToastContainer } from "react-toastify"
 import LoginSignup from '../components/login-signup/loginSignup'
 import { Provider } from "react-redux";
 import rootStore from "../store";
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
 
 export default function Home() {
   return (
